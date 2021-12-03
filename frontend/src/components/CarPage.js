3423a759ed9ae2a073aa6d5f2749b0ca39033256
Mariana Lozynska <marianalozynska@YPs-iMac.local>
2021-12-03 18:17:58 +0000

    navBar

--- frontend/src/components/CarPage.js
+++ frontend/src/components/CarPage.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import { Segment, Image } from 'semantic-ui-react'
 
 const CarPage = () => {
   const { id } = useParams()
@@ -11,24 +12,45 @@ const CarPage = () => {
     const getData = async () => {
       const { data } = await axios.get(`/api/cars/${id}`)
       setCar(data)
-      console.log(car)
+      
     }
     getData()
     
   }, [id])
-
+  console.log(car)
   return (
     <>
-      {car ? 
-        // <h1>{car.title}</h1>
-        <h1>{car.description}</h1>
+      {car ?
+        <Segment>
+          <h1>{car.title}</h1>
+          <p>{car.description}</p>
+          <h5>{car.colour}</h5>
+          <h5>{car.minprice}</h5>
+          <h5>{car.hourly}</h5>
+          <h5>{car.passengers}</h5>
+          <h5>{car.steering}</h5>
+          <h5>{car.year}</h5>
+          <h5>{car.steering}</h5>
+          <Image src = {car.picture}></Image>
+          <h5>{}</h5>
+          <h5>{}</h5>
+          <Segment>
+            <p>{car.services[0].description}</p>
+            {/* {car.services.map( service => {
+              return (
+                <p key={id}>{service}</p>
+              )
+              
+            })} */}
+          </Segment>
+        </Segment>
+        
         
 
         :
         <h1> Loading</h1>
-      }
       
-      <h1>Car Page</h1>
+      }
     </>
   )
 
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,37 +1,42 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { Segment, Menu, MenuItem, Icon, Header, Grid } from 'semantic-ui-react'
+import { Segment, Menu, MenuItem, Icon, Header, Grid, Image } from 'semantic-ui-react'
 
 const NavBar = () => {
   const history = useHistory()
 
   return (
     <>
-      <Segment color="yellow" inverted size="tiny">
-        <Header as="h1" style={{ margin: '0' }}>VintCars 🚗</Header>
-        <Menu icon secondary color="yellow" inverted style={{ margin: '0' }}>
+      <Segment id="navBarSegment" basic>
+        <Grid>
+          {/* style={{ color: '#e5cf89' }} */}
+          <Image id="navBarImg" src={'https://www.freeiconspng.com/thumbs/vintage-cars-png/volkswagen-vintage-cars-png-0.png'}/>
+          <Header as="h1" style={{ margin: '10px 0 0 0' }}>VintCars</Header>
+
+          
+        </Grid>
+        
+        <Menu secondary color="yellow" inverted>
           <MenuItem position='left'>
             <Menu.Item onClick={() => history.push('/')}>
-              <Icon name="home" size="big" color="black"/>
+              <Icon name="home" size="large" color="black"/>
             </Menu.Item>
-          </MenuItem>
-          <MenuItem position="center">
             <Menu.Item onClick={() => history.push('/cars')}>
               <h3>Our Cars</h3>
             </Menu.Item>
           </MenuItem>
-          <MenuItem position="right">
-            <Grid.Column>
+          <MenuItem className="navInfo" position="right">
+            <Grid.Column style={{ padding: '0' }}>
               <Menu.Item style={{ padding: '0' }}>
-                <h3 >Mon-Sat 9am - 6pm</h3>
+                <h5 style={{ color: 'black' }}>Mon-Sat 9am - 6pm</h5>
               </Menu.Item>
               <Menu.Item style={{ padding: '0' }}>
-                <Icon name="phone" size="big" color="black"/>
-                <p>0123456789</p>
+                <Icon name="phone" size="small" color="black"/>
+                <p className="pNavBar">0123456789</p>
               </Menu.Item>
               <Menu.Item style={{ padding: '0' }}>
-                <Icon name="mail outline" size="big" color="black"/>
-                <p>info@vintCars.com</p>
+                <Icon name="mail outline" size="small" color="black"/>
+                <p className="pNavBar">info@vintCars.com</p>
               </Menu.Item>
             </Grid.Column>
           </MenuItem>
