8490015282c64006fdbbbc894a4dcc0de988ce97
Mariana Lozynska <marianalozynska@YPs-iMac.local>
2021-12-03 13:34:19 +0000

    received info for carPage

--- frontend/src/components/CarPage.js
+++ frontend/src/components/CarPage.js
@@ -1,25 +1,33 @@
-import React from 'react'
-// import React, { useState, useEffect } from 'react'
-// import axios from 'axios'
-// import { useParams } from 'react-router-dom'
+// import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 const CarPage = () => {
-  // const { id } = useParams()
-  // const [car, setCar] = useState(null)
+  const { id } = useParams()
+  const [car, setCar] = useState(null)
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const { data: car } = await axios.get(`/api/cars/${id}`)
-  //     setCar(car)
-  //     console.log(car)
-  //   }
-  //   getData()
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(`/api/cars/${id}`)
+      setCar(data)
+      console.log(car)
+    }
+    getData()
     
-  // }, [id])
+  }, [id])
 
   return (
     <>
-      {/* <h1>{car.title}</h1> */}
+      {car ? 
+        // <h1>{car.title}</h1>
+        <h1>{car.description}</h1>
+        
+
+        :
+        <h1> Loading</h1>
+      }
+      
       <h1>Car Page</h1>
     </>
   )
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,25 +1,41 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { Segment, Menu, MenuItem, Icon, Header } from 'semantic-ui-react'
+import { Segment, Menu, MenuItem, Icon, Header, Grid } from 'semantic-ui-react'
 
 const NavBar = () => {
   const history = useHistory()
 
   return (
     <>
-      <Segment color="yellow" inverted>
-        <Header as="h1" inverted className="navbarTitle">VintCars 🚗</Header>
-        <Menu icon secondary color="yellow" inverted>
+      <Segment color="yellow" inverted size="tiny">
+        <Header as="h1" style={{ margin: '0' }}>VintCars 🚗</Header>
+        <Menu icon secondary color="yellow" inverted style={{ margin: '0' }}>
           <MenuItem position='left'>
             <Menu.Item onClick={() => history.push('/')}>
-              <Icon name="home" size="big" />
-            </Menu.Item>
-            <Menu.Item onClick={() => history.push('/cars')}>
-              <p>Our Cars</p>
+              <Icon name="home" size="big" color="black"/>
             </Menu.Item>
           </MenuItem>
+          <MenuItem position="center">
+            <Menu.Item onClick={() => history.push('/cars')}>
+              <h3>Our Cars</h3>
+            </Menu.Item>
+          </MenuItem>
+          <MenuItem position="right">
+            <Grid.Column>
+              <Menu.Item style={{ padding: '0' }}>
+                <h3 >Mon-Sat 9am - 6pm</h3>
+              </Menu.Item>
+              <Menu.Item style={{ padding: '0' }}>
+                <Icon name="phone" size="big" color="black"/>
+                <p>0123456789</p>
+              </Menu.Item>
+              <Menu.Item style={{ padding: '0' }}>
+                <Icon name="mail outline" size="big" color="black"/>
+                <p>info@vintCars.com</p>
+              </Menu.Item>
+            </Grid.Column>
+          </MenuItem>
         </Menu>
-        <h1>NavBar lives here</h1>
       </Segment>
 
 
