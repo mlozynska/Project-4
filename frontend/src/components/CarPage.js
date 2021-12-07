a9fcfe4a1308eb9b19fa5a0cb212a0600159fa8d
Mariana Lozynska <marianalozynska@YPs-iMac.local>
2021-12-07 13:07:35 +0000

    review is working

--- frontend/src/components/CarPage.js
+++ frontend/src/components/CarPage.js
@@ -5,24 +5,56 @@ import { useParams } from 'react-router-dom'
 import CarSpec from './CarSpec'
 import EnqForm from './EnqForm'
 import Services from './Services'
-import { Segment,  Header, Divider, Comment, Rating } from 'semantic-ui-react'
+import { Segment,  Header, Divider, Comment, Rating, Form, Button } from 'semantic-ui-react'
 import Carousel from 'react-responsive-carousel/lib/js/components/Carousel/index'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 const CarPage = () => {
   const { id } = useParams()
   const [car, setCar] = useState(null)
+  const [review, setReview] = useState({
+    name: '',
+    title: '',
+    text: '',
+    rating: '',
+    car: `${id}`,
+  })
+  // const [newReview, setNewReview] = useState(false)
 
   useEffect(() => {
     const getData = async () => {
       const { data } = await axios.get(`/api/cars/${id}`)
       setCar(data)
-      console.log(data.image_set.image)
+      
     }
     getData()
     
   }, [id])
   // console.log(car)
+
+  const handleChange = (event) => {
+    const newReview = { ...review, [event.target.name]: event.target.value }
+    setReview(newReview)
+  }
+  
+  const handleStars = e => {
+    const rating = e.target.attributes.getNamedItem('aria-posinset').value
+    console.log(rating)
+    const newReview = { ...review, rating }
+    setReview(newReview)
+  }
+
+  const handleSubmit = async (event) => {
+    console.log(review)
+    event.preventDefault()
+    try {
+      await axios.post('/api/reviews/', review)
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
   return (
     <Segment id="carPageSegment" inverted basic>
       {car ?
@@ -94,6 +126,41 @@ const CarPage = () => {
                       Be first to comment and rate this park!
                     </Header>
                   )}
+
+                  <Form reply>
+                    <input
+                      onChange={handleChange}
+                      name="name"
+                      placeholder="Your name"
+                    />
+                    <input
+                      onChange={handleChange}
+                      name="title"
+                      placeholder="Title"
+                    />
+                    <Form.TextArea
+                      onChange={handleChange}
+                      name="text"
+                      placeholder="Your review..."
+                    />
+                    <p style={{ color: 'black' }}>
+                      Please add a rating to submit your comment
+                    </p>
+                    <Rating
+                      onClick={handleStars}
+                      icon="star"
+                      maxRating={5}
+                      name="rating"
+                    />
+                    
+                    <Button
+                      autoFocus
+                      onClick={handleSubmit}
+                      content="Add Review"
+                      labelPosition="left"
+                    />
+                  </Form>
+
                 </Comment.Content>
               </Comment>
             </Comment.Group>
--- frontend/src/components/CarSpec.js
+++ frontend/src/components/CarSpec.js
@@ -6,37 +6,43 @@ const CarSpec = ({ car }) => {
   return  (
     <Segment style={{ background: '#7b1113' }} basic>
       <Card.Group style={{ justifyContent: 'center' }} items={5}>
+
         <Card id="infoBox">
           <Card.Content style={{ background: 'black' }} textAlign="center">
             <Image id="icon" src={'https://www.bookaclassic.co.uk/images/calendar.png'} size="mini" />
-            <h4 textalign='center' style={{ color: '#d4af37' }}>Year: {car.year}</h4>
+            <h6 id="h6CarSpec" textalign='center' >Year: {car.year}</h6>
           </Card.Content>
         </Card>
+
         <Card id="infoBox">
           <Card.Content style={{ background: 'black' }} textAlign="center">
             <Image id="icon" src={'https://www.bookaclassic.co.uk/images/people.png'} size="mini"/>
-            <h4 textalign='center' style={{ color: '#d4af37' }}>Passengers: {car.passengers}</h4>
+            <h6 id="h6CarSpec" textalign='center'>Passengers: {car.passengers}</h6>
           </Card.Content>
         </Card>
+
         <Card id="infoBox">
           <Card.Content style={{ background: 'black' }} textAlign="center">
             <Image id="icon" src={'https://www.bookaclassic.co.uk/images/paint.png'} size="mini" />
-            <h4 textalign='center' style={{ color: '#d4af37' }}>Colour: {car.colour}</h4>
+            <h6 id="h6CarSpec" textalign='center'>Colour: {car.colour}</h6>
           </Card.Content>
         </Card>
+
         <Card id="infoBox">
           <Card.Content style={{ background: 'black' }} textAlign="center">
             <Image id="icon" src={'https://www.bookaclassic.co.uk/images/wheel2.png'} size="mini" />
-            <h4 textalign='center' style={{ color: '#d4af37' }}>Steering: {car.steering}</h4>
+            <h6 id="h6CarSpec" textalign='center'>Steering: {car.steering}</h6>
           </Card.Content>
         </Card>
+
         <Card id="infoBox">
           <Card.Content style={{ background: 'black' }} textAlign="center">
             <Image id="money" src={'https://lh3.googleusercontent.com/proxy/-1zOfkCoBxVWdSx-YU6mBVFXkBW_ErbDxHWNALQy8YpfrnIf6xMS912j-jge1GXC6dXnFpHePTZXyZCCWIh7ykbZcapzZoGU-eauWD8puB9FqxaVeaoR4UlNtuQl35lw1ny29i2SY-U'} />
-            <h4 textalign='center' style={{ color: '#d4af37' }}>Min.price: £ {car.minprice}</h4>
-            <h4 textalign='center' style={{ color: '#d4af37' }}>Hourly: £ {car.hourly}</h4>
+            <h6 id="h6CarSpec" textalign='center'>Min.price: £ {car.minprice}</h6>
+            <h6 id="h6CarSpec" textalign='center'>Hourly: £ {car.hourly}</h6>
           </Card.Content>
         </Card>
+
       </Card.Group>
     </Segment>  
   )
