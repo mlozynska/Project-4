92a4df99cab20dd85a446d2fe84a7f6cf05b6c16
Mariana Lozynska <marianalozynska@YPs-iMac.local>
2021-12-21 11:35:29 +0000

    new changes

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-p!dnft67&2%4ml(wk9tzjkg3h00e38u7a9dauue3*w^qlaf)3d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
